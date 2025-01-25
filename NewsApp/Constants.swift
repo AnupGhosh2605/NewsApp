0b84337e56648833d15e90e5c23a17da95bd9d1a
AnupGhosh2605 <anup.19ghosh@gmail.com>
2025-01-26 00:01:41 +0530

    Removed the API Key

--- NewsApp/Constants.swift
+++ NewsApp/Constants.swift
@@ -11,7 +11,7 @@ import Foundation
 
 
 struct Constants {
-   static let API_KEY = "b09e3d651a95441aa842b38921f5a818"
+   static let API_KEY = ""
    static let API_URL = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=\(API_KEY)"
    static let NavBarTitle = "Daily Headlines"
 }
