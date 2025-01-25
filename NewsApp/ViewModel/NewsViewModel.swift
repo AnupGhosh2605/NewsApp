4a3337ebc3dd598fbac1f2c9f82a29469e5c4464
AnupGhosh2605 <anup.19ghosh@gmail.com>
2025-01-25 22:36:36 +0530

    Image Cache Updated

--- NewsApp/ViewModel/NewsViewModel.swift
+++ NewsApp/ViewModel/NewsViewModel.swift
@@ -12,13 +12,37 @@ import UIKit
 class NewsViewModel {
        
     var onFetchData: ((NewsData?) -> Void)?   //  Call back to update the view
-    
+    private let imageCache = NSCache<NSString, UIImage>()
     private let network: NetworkServiceProtocol
 
     init(networkService : NetworkServiceProtocol = NetworkService.instace ) {
         self.network = networkService
+        imageCache.countLimit = 20                     // Maximum number of items in the cache
+        imageCache.totalCostLimit = 20 * 1024 * 1024   // Maximum memory cost in bytes
+        
+        // Add Observers to check if app is using too much memory
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(clearCache),
+            name: UIApplication.didReceiveMemoryWarningNotification,
+            object: nil
+        )
+        
+        // Cehcks if the app goes to background from the foreground.
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(clearCache),
+            name: UIApplication.didEnterBackgroundNotification,
+            object: nil
+        )
     }
     
+    // It'll clear the cache
+    @objc private func clearCache() {
+        imageCache.removeAllObjects()
+        print("Cache cleared")
+    }
+
     // Func to fetch the news data by calling the method from Network Service
     func fetchNewsData() {
         network.downloadData(urlString: Constants.API_URL) { returnedNewsData in
@@ -50,11 +74,23 @@ class NewsViewModel {
     
     // Fetch the image data from URL anc convert it to UIImage
     func fetchImageData(imageUrl : String,completion: @escaping (UIImage?) -> Void) {
+        
+        // Check is image is present in the cache
+        if let cachedImage = imageCache.object(forKey: imageUrl as NSString) {
+            completion(cachedImage)
+            return
+        }
+        
         network.downloadData(urlString: imageUrl) { returnedImageData in
             switch returnedImageData {
             case .success(let data):
                 if let image = UIImage(data: data) {
+                    // Storing image with the key
+                    self.imageCache.setObject(image, forKey: imageUrl as NSString)
                     completion(image)
+                }
+                else {
+                    completion(nil)
                 }
             case .failure(let failure):
                 print("Error fetching imageData: \(failure)")
